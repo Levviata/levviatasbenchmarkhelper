e836094554fd93b8c18c00ad41be31d4986e066c
Levviata <68709348+Levviata@users.noreply.github.com>
2025-01-22 13:34:00 +0100

    refactor: Change default project identifiers to ours

--- src/main/java/com/levviata/benchmarkhelper/BenchmarkHelper.java
+++ src/main/java/com/levviata/benchmarkhelper/BenchmarkHelper.java
@@ -1,13 +1,14 @@
-package com.example.modid;
+package com.levviata.benchmarkhelper;
 
-import com.example.modid.Tags;
+import com.levviata.benchmarkhelper.listeners.MovementHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class BenchmarkHelper {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
@@ -19,6 +20,7 @@ public class ExampleMod {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+        MinecraftForge.EVENT_BUS.register(new MovementHandler());
     }
 
 }
